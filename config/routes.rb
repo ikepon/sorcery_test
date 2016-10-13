249a7356dc438b9820da12a18505965dc43bd997
ikepon <tatsuyanoheya@gmail.com>
2016-10-13 16:04:47 +0900

    パスワードリセット機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       get :activate
     end
   end
+  resources :password_resets
 
   get 'login' => 'user_sessions#new', as: :login
   get 'logout' => 'user_sessions#destroy', as: :logout
