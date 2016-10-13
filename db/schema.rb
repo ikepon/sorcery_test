adf3b3b3cc185b04b51b487dcd5d200b9f379e68
ikepon <tatsuyanoheya@gmail.com>
2016-10-13 14:55:24 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161013032221) do
+ActiveRecord::Schema.define(version: 20161013055412) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",            null: false
+    t.string   "email",                        null: false
     t.string   "crypted_password"
     t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "remember_me_token"
+    t.datetime "remember_me_token_expires_at"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
   end
 
 end
